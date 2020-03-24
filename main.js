0d433a143d4b27ff3e9d49902cb15dd8f65cd168
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-03-24 17:10:27 -0400

    outlined controls based off of original WinForms app

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const path = require('path')
 const window = require('electron-window')
 
 app.on('ready', () => {
-  const mainWindow = window.createWindow({ width: 600, height: 400 })
+  const mainWindow = window.createWindow({ width: 600, height: 500, webPreferences: { webSecurity: true, nodeIntegration: true } })
   const someArgs = { data: 'hi' }
-  const indexPath = path.resolve(__dirname, '..', 'weird-location', 'index.html')
+  const indexPath = path.resolve(__dirname, 'index.html')
 
   mainWindow.showUrl(indexPath, someArgs, () => {
     console.log('window is now visible!')
