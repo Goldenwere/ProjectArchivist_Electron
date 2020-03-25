d4248ca0cb362b0bdca925208d83f3693feebb63
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-03-25 17:09:32 -0400

    outline page setup for item properties window

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 const { app } = require('electron')
 const path = require('path')
 const window = require('electron-window')
-
+/*
 app.on('ready', () => {
   const mainWindow = window.createWindow({
     backgroundColor: "#141414",
@@ -16,6 +16,20 @@ app.on('ready', () => {
   });
   const someArgs = { data: 'hi' }
   const indexPath = path.resolve(__dirname, 'index.html')
+*/
+/* To be removed */
+app.on('ready', () => {
+  const mainWindow = window.createWindow({
+    backgroundColor: "#141414",
+    width: 800,
+    height: 400,
+    webPreferences: {
+      webSecurity: true,
+      nodeIntegration: true
+    }
+  });
+  const someArgs = { data: 'hi' }
+  const indexPath = path.resolve(__dirname, 'itemprop.html')
 
   mainWindow.showUrl(indexPath, someArgs, () => {
     console.log('window is now visible!')
