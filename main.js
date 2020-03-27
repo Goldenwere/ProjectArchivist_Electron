8aed6d9d490f295fa22b1944269b216a95935b46
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-03-26 23:12:25 -0400

    finished creating exclusion properties window

--- main.js
+++ main.js
@@ -17,8 +17,10 @@ app.on('ready', () => {
   const someArgs = { data: 'hi' }
   const indexPath = path.resolve(__dirname, 'index.html')
 */
+
 /* To be removed */
-app.on('ready', () => {
+
+/*app.on('ready', () => {
   const mainWindow = window.createWindow({
     backgroundColor: "#141414",
     width: 800,
@@ -30,7 +32,20 @@ app.on('ready', () => {
   });
   const someArgs = { data: 'hi' }
   const indexPath = path.resolve(__dirname, 'itemprop.html')
+*/
 
+app.on('ready', () => {
+  const mainWindow = window.createWindow({
+    backgroundColor: "#141414",
+    width: 500,
+    height: 200,
+    webPreferences: {
+      webSecurity: true,
+      nodeIntegration: true
+    }
+  });
+  const someArgs = { data: 'hi' }
+  const indexPath = path.resolve(__dirname, 'exclusionprop.html')
   mainWindow.showUrl(indexPath, someArgs, () => {
     console.log('window is now visible!')
   })
