56085f4cab1a1e042cf25646d2074c8058aa685c
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-04-12 18:23:28 -0400

    handled defaults and removed application from namespace

--- src/script/application.ts
+++ src/script/application.ts
@@ -1,57 +1,60 @@
 import $ from "jquery";
 
-namespace ProjectArchivist {
-  let mainPage: JQuery<HTMLElement>;
-  let itemPage: JQuery<HTMLElement>;
-  let exclPage: JQuery<HTMLElement>;
+let mainPage: JQuery<HTMLElement>;
+let itemPage: JQuery<HTMLElement>;
+let exclPage: JQuery<HTMLElement>;
+let items: ProjectArchivist.ArchivedItem[];
+let workingIndex: number;
 
-  $(document).ready(function() {
-    mainPage = $("#Page_Main");
-    itemPage = $("#Page_Item");
-    exclPage = $("#Page_Excl");
+$(document).ready(function() {
+  mainPage = $("#Page_Main");
+  itemPage = $("#Page_Item");
+  exclPage = $("#Page_Excl");
 
-    mainPage.toggleClass("current-page");
+  mainPage.toggleClass("current-page");
+  workingIndex = 0;
+  items = [];
 
-    $("#Button_ListItems_Add").click(function() {
-      switchToFromItemPage();
-    });
-
-    $("#Button_ListItems_Edit").click(function() {
-      switchToFromItemPage();
-    });
-
-    $("#Button_Exclusions_Add").click(function() {
-      switchToFromExclPage();
-    });
-
-    $("#Button_Exclusions_Edit").click(function() {
-      switchToFromExclPage();
-    });
-
-    $("#Button_AddItem_SaveExit").click(function() {
-      switchToFromItemPage();
-    });
-
-    $("#Button_AddItem_NoSaveExit").click(function() {
-      switchToFromItemPage();
-    });
-
-    $("#Button_AddExcl_SaveExit").click(function() {
-      switchToFromExclPage();
-    });
-
-    $("#Button_AddExcl_NoSaveExit").click(function() {
-      switchToFromExclPage();
-    });
+  $("#Button_ListItems_Add").click(function() {
+    switchToFromItemPage();
   });
 
-  function switchToFromItemPage() {
-    mainPage.toggleClass("current-page");
-    itemPage.toggleClass("current-page");
-  }
+  $("#Button_ListItems_Edit").click(function() {
+    switchToFromItemPage();
+    items[workingIndex] = new ProjectArchivist.ArchivedItem();
+  });
 
-  function switchToFromExclPage() {
-    itemPage.toggleClass("current-page");
-    exclPage.toggleClass("current-page");
-  }
+  $("#Button_Exclusions_Add").click(function() {
+    switchToFromExclPage();
+  });
+
+  $("#Button_Exclusions_Edit").click(function() {
+    switchToFromExclPage();
+  });
+
+  $("#Button_AddItem_SaveExit").click(function() {
+    switchToFromItemPage();
+  });
+
+  $("#Button_AddItem_NoSaveExit").click(function() {
+    switchToFromItemPage();
+  });
+
+  $("#Button_AddExcl_SaveExit").click(function() {
+    switchToFromExclPage();
+  });
+
+  $("#Button_AddExcl_NoSaveExit").click(function() {
+    switchToFromExclPage();
+  });
+});
+
+function switchToFromItemPage() {
+  mainPage.toggleClass("current-page");
+  itemPage.toggleClass("current-page");
+}
+
+function switchToFromExclPage() {
+  itemPage.toggleClass("current-page");
+  exclPage.toggleClass("current-page");
 }
--- src/script/classes.ts
+++ src/script/classes.ts
@@ -18,6 +18,25 @@ namespace ProjectArchivist {
   }
 
   /*
+  ** An object used for automatically sending default values to an ArchivedItem if they are defined
+  */
+  export class Globals {
+    password: string;
+    type: ArchiveType;
+    level: number;
+    method: CompressionMethod;
+    destination: string;
+
+    constructor(_password: string, _type: ArchiveType, _level: number, _method: CompressionMethod, _destination: string) {
+      this.password = _password;
+      this.type = _type;
+      this.level = _level;
+      this.method = _method;
+      this.destination = _destination;
+    }
+  }
+
+  /*
   ** An archived item represents the resulting file archive that 7-Zip would create.
   ** The information stored in an archived item outlines the command-line parameters for 7-Zip to use.
   */
@@ -52,17 +71,28 @@ namespace ProjectArchivist {
     // In 7-Zip parameters: -m0=; the algorithm used for compression (simply ignored by 7-Zip if compressionLevel is 0)
     compressionMethod: CompressionMethod;
 
-    constructor(_itemName: string, _sourcePath: string, _destinationPath: string, _fileName: string, _password: string, _exclusions: string[], _exclusionsRecursives: string[], _type: ArchiveType, _compressionLevel: number, _compressionMethod: CompressionMethod) {
-      this.itemName = _itemName;
-      this.sourcePath = _sourcePath;
-      this.destinationPath = _destinationPath;
-      this.fileName = _fileName;
-      this.password = _password;
-      this.exclusions = _exclusions;
-      this.exclusionsRecursives = _exclusionsRecursives;
-      this.type = _type;
-      this.compressionLevel = _compressionLevel;
-      this.compressionMethod = _compressionMethod;
+    constructor(defaultValues?: Globals) {
+      this.itemName = "";
+      this.sourcePath = "";
+      this.fileName = "";
+      this.exclusions = [];
+      this.exclusionsRecursives = [];
+
+      if (defaultValues != undefined) {
+        this.password = defaultValues.destination;
+        this.destinationPath = defaultValues.destination;
+        this.type = defaultValues.type;
+        this.compressionLevel = defaultValues.level;
+        this.compressionMethod = defaultValues.method;
+      }
+
+      else {
+        this.password = "";
+        this.destinationPath = "";
+        this.type = Defaults.DefaultType;
+        this.compressionMethod = Defaults.DefaultMethod;
+        this.compressionLevel = Defaults.DefaultLevel;
+      }
     }
   }
 }
